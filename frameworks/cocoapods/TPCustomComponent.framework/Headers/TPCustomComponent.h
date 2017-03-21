3bec282066b04db580e352fe3cf631bb4cbb8c1d
滕盼 <455462982@qq.com>
2017-03-21 18:05:10 +0800

    xxxx

---
+++ frameworks/cocoapods/TPCustomComponent.framework/Headers/TPCustomComponent.h
@@ -0,0 +1,20 @@
+//
+//  TPCustomComponent.h
+//  TPCustomComponent
+//
+//  Created by sfit0194 on 3/17/17.
+//  Copyright © 2017 sfit0194. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPCustomComponent.
+FOUNDATION_EXPORT double TPCustomComponentVersionNumber;
+
+//! Project version string for TPCustomComponent.
+FOUNDATION_EXPORT const unsigned char TPCustomComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPCustomComponent/PublicHeader.h>
+
+#import <TPCustomComponent/TPCustomManager.h>
+
---
+++ frameworks/cocoapods/TPCustomComponent.framework/Headers/TPCustomManager.h
@@ -0,0 +1,15 @@
+//
+//  TPCustomManager.h
+//  TPCustomComponent
+//
+//  Created by sfit0194 on 3/17/17.
+//  Copyright © 2017 sfit0194. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TPCustomManager : NSObject
+
+- (NSString *)subManagerClassName;
+
+@end
